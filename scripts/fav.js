e7624e9615f060b285af3c25e6281c3ce43ad70f
AbhishekV9 <avishekv444@gmail.com>
2021-08-26 17:50:07 +0530

    adding favourites to the dom

---
+++ scripts/fav.js
@@ -0,0 +1,38 @@
+const favMeals=JSON.parse(localStorage.getItem('favMeals'));
+mealsList=document.getElementById("meals-list");
+let url='https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+
+const fetchData=async (mealId)=>{
+    try {
+        let res=await fetch(url+mealId);
+        let data=await res.json();
+        displayResults(data.meals[0]);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+const showFav=()=>{
+    if(favMeals.length===0){
+        mealsList.innerHtml='<h1>No Favourite Meals Present</h1>'
+    }else{
+        favMeals.map((mealId)=>{
+            const mealData=fetchData(mealId);
+        })
+    }
+}
+
+const displayResults=(meal)=>{
+
+    mealsList.innerHTML +=`<li class="meal">
+    <img src="${meal.strMealThumb}" /img>
+     <div class="meal-name" id="${meal.idMeal}">
+     <h2 class="recipe-name">${meal.strMeal}</h2> 
+     <i class="${ isFav ? 'fas' : 'far' } fa-heart fav-btn"></i>
+     </div>
+     </li>`;
+}
+
+showFav();
+
